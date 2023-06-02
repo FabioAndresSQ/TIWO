3dea6945d232f7e3abf6bedd553b911b191630d0
Fabio Andres <fabi0andr3s@gmail.com>
2023-06-02 14:01:48 -0500

    App viewBiding fixed on WorkoutsAdapter

--- app/src/main/java/com/faesfa/tiwo/WorkoutsAdapter.kt
+++ app/src/main/java/com/faesfa/tiwo/WorkoutsAdapter.kt
@@ -5,10 +5,9 @@ import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
-import de.hdodenhof.circleimageview.CircleImageView
+import com.faesfa.tiwo.databinding.ItemViewBinding
 
 class WorkoutsAdapter(
     private val context: Context, val items: ArrayList<WorkoutsModelClass>,
@@ -25,32 +24,32 @@ class WorkoutsAdapter(
         val item = items[position]
 
         when (item.category){
-            "Chest" -> {holder.workoutImage.setImageResource(R.drawable.chest_ic)}
-            "Back" -> {holder.workoutImage.setImageResource(R.drawable.back_ic)}
-            "Shoulder" -> {holder.workoutImage.setImageResource(R.drawable.shoulder_ic)}
-            "Arms" -> {holder.workoutImage.setImageResource(R.drawable.arm_ic)}
-            "Legs" -> {holder.workoutImage.setImageResource(R.drawable.legs_ic)}
-            "Abs" -> {holder.workoutImage.setImageResource(R.drawable.abs_ic)}
+            "Chest" -> {holder.binding.workoutImage.setImageResource(R.drawable.chest_ic)}
+            "Back" -> {holder.binding.workoutImage.setImageResource(R.drawable.back_ic)}
+            "Shoulder" -> {holder.binding.workoutImage.setImageResource(R.drawable.shoulder_ic)}
+            "Arms" -> {holder.binding.workoutImage.setImageResource(R.drawable.arm_ic)}
+            "Legs" -> {holder.binding.workoutImage.setImageResource(R.drawable.legs_ic)}
+            "Abs" -> {holder.binding.workoutImage.setImageResource(R.drawable.abs_ic)}
         }
         val dataManager = DataManager()
 
 
         @RequiresApi(Build.VERSION_CODES.M)
-        holder.workoutImage.borderColor = context.getColor(R.color.AppColor)
-        holder.workoutImage.borderWidth = 5
+        holder.binding.workoutImage.borderColor = context.getColor(R.color.AppColor)
+        holder.binding.workoutImage.borderWidth = 5
 
-        holder.workoutName.text = item.name.uppercase()
-        holder.sets.text = item.sets.toString()
+        holder.binding.workoutNameTxt.text = item.name.uppercase()
+        holder.binding.setsTxt.text = item.sets.toString()
         //Check if working with Reps or Time
         if (item.reps){
-            holder.workWith.text = context.getString(R.string.itemRepsTxt)
-            holder.reps.text = item.num_reps.toString()
-            holder.workoutDuration.text = dataManager.convertTime(((item.reps_time * item.num_reps + item.rest_time) * item.sets).toInt())
+            holder.binding.workWithTxt.text = context.getString(R.string.itemRepsTxt)
+            holder.binding.repsTxt.text = item.num_reps.toString()
+            holder.binding.workoutDurationTxt.text = dataManager.convertTime(((item.reps_time * item.num_reps + item.rest_time) * item.sets).toInt())
         }else{
             val repsText = dataManager.convertTime(item.work_time)
-            holder.workWith.text = context.getString(R.string.itemWorkTxt)
-            holder.reps.text = repsText
-            holder.workoutDuration.text = dataManager.convertTime((item.work_time + item.rest_time) * item.sets)//convertTime((item.work_time + item.rest_time) * item.sets)
+            holder.binding.workWithTxt.text = context.getString(R.string.itemWorkTxt)
+            holder.binding.repsTxt.text = repsText
+            holder.binding.workoutDurationTxt.text = dataManager.convertTime((item.work_time + item.rest_time) * item.sets)//convertTime((item.work_time + item.rest_time) * item.sets)
         }
     }
 
@@ -59,14 +58,8 @@ class WorkoutsAdapter(
     }
 
     inner class ViewHolder(view: View): RecyclerView.ViewHolder(view), View.OnClickListener{
-
+        val binding = ItemViewBinding.bind(view)
         //val workoutImage: ImageView = view.findViewById(R.id.workoutImage)
-        val workoutName: TextView = view.findViewById(R.id.workoutNameTxt)
-        val sets: TextView = view.findViewById(R.id.setsTxt)
-        val workWith: TextView = view.findViewById(R.id.workWithTxt)
-        val reps: TextView = view.findViewById(R.id.repsTxt)
-        val workoutDuration: TextView = view.findViewById(R.id.workoutDurationTxt)
-        val workoutImage: CircleImageView = view.findViewById(R.id.workoutImage)
 
         init {
             itemView.setOnClickListener(this)
